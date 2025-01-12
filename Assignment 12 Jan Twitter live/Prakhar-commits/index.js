c47ed166b02643d50495559651a1f79c0196e7fb
Prakhar <prakharb56@gmail.com>
2025-01-12 22:14:38 +0530

    feat: search

--- Assignment 12 Jan Twitter live/Prakhar-commits/index.js
+++ Assignment 12 Jan Twitter live/Prakhar-commits/index.js
@@ -3,20 +3,34 @@ let events = [];
 async function getData() {
   const data = await fetch("data.json");
   const dataSet = await data.json();
+  events = dataSet;
   console.log(dataSet);
-
   const container = document.getElementById("events");
-  container.innerHTML = dataSet
+  container.innerHTML = dataSet.map(
+    (event) => `<div><h3>${event.title}</h3>
+    <p>${event.description}</p></div>`
+  );
+}
+
+function fileteredEvents() {
+  const inputValue = document.getElementById("search").value.toLowerCase();
+  let fileteredEvents = events.filter((event) =>
+    event.title.toLowerCase().includes(inputValue)
+  );
+  console.log(fileteredEvents);
+  const container = document.getElementById("events");
+  container.innerHTML = fileteredEvents
     .map(
-      (event) => `<div><h3>${event.title}</h3><p>${event.description}</p></div>`
+      (event) => `<div><h3>${event.title}</h3>
+    <p>${event.description}</p></div>`
     )
     .join("");
 }
+
+document.getElementById("search").addEventListener("input", () => {
+  fileteredEvents();
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   getData();
 });
-
-function getInputValue() {
-  const inputValue = document.getElementById("search").value.toLowerCase();
-  return inputValue;
-}
