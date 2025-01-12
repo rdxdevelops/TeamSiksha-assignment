e1e8155f0a260076f9f07a42c81d4c0e2e51af1f
Prakhar <prakharb56@gmail.com>
2025-01-12 22:52:49 +0530

    added sorting

--- Assignment 12 Jan Twitter live/Prakhar-commits/index.js
+++ Assignment 12 Jan Twitter live/Prakhar-commits/index.js
@@ -1,18 +1,21 @@
 let events = [];
+let currentPage = 1;
+const eventsPerPage = 10;
+let sortDirection = "desc";
 
 async function getData() {
   const data = await fetch("data.json");
-  const dataSet = await data.json();
-  events = dataSet;
-  console.log(dataSet);
+  events = await data.json();
+  reenderedEvents();
+  console.log(events);
   const container = document.getElementById("events");
-  container.innerHTML = dataSet.map(
+  container.innerHTML = events.map(
     (event) => `<div><h3>${event.title}</h3>
     <p>${event.description}</p></div>`
   );
 }
 
-function fileteredEvents() {
+function reenderedEvents() {
   const inputValue = document.getElementById("search").value.toLowerCase();
   let fileteredEvents = events.filter((event) =>
     event.title.toLowerCase().includes(inputValue)
@@ -25,10 +28,24 @@ function fileteredEvents() {
     <p>${event.description}</p></div>`
     )
     .join("");
+
+  fileteredEvents.sort((a, b) => {
+    const dateA = new Date(a.startTime);
+    const dateB = new Date(b.startTime);
+    return sortDirection === "asc" ? dateA - dateB : dateB - dateA;
+  });
+  //   console.log(sorted);
 }
 
 document.getElementById("search").addEventListener("input", () => {
   fileteredEvents();
+});
+document.getElementById("sortButton").addEventListener("click", () => {
+  sortDirection = sortDirection === "asc" ? "desc" : "asc";
+  document.getElementById("sortButton").textContent = `Sort by Date ${
+    sortDirection === "asc" ? "up" : "downn"
+  }`;
+  reenderedEvents();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
